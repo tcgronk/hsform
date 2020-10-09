6ae1bbfdd6c0e5f32f9f27dc85279e4c8d9d0ac7
Tess Gronski <tessgronski@gmail.com>
2020-10-08 23:42:44 -0700

    Adds hb script

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// const hsApiKey=""
+const hsApiKey="b5caaf7a-3349-4fcd-9c7c-de0776f99628"
+const hsURL="https://app.hubspot.com/oauth/authorize?"
 
 function hubspotForm(){
     $("#contactForm").submit(event=>{
