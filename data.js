3c6b6999e857629ac455722fe531b10e5d3a25ed
Tess Gronski <tessgronski@gmail.com>
2020-10-10 11:17:08 -0700

    Script test

--- data.js
+++ data.js
@@ -1,13 +1,19 @@
 "use strict";
 
-const hsApiKey="b5caaf7a-3349-4fcd-9c7c-de0776f99628"
-const hsURL="https://app.hubspot.com/oauth/authorize?"
-
 function hubspotForm(){
     $("#contactForm").submit(event=>{
         event.preventDefault();
+        apiTest();
         $("#formResponse").removeClass("hidden").text("Form Submitted")
 
+    })
+    
+
+}
+
+function apiTest(){
+    $.post( "https://js.hs-banner.com/8612571.js",function(){
+        alert("success")
     })
 }
 
