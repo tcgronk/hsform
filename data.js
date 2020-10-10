8d2d6d4b4695d528c219bc99623e329bf73c506e
Tess Gronski <tessgronski@gmail.com>
2020-10-10 12:10:09 -0700

    Removes testing

--- data.js
+++ data.js
@@ -3,18 +3,11 @@
 function hubspotForm(){
     $("#contactForm").submit(event=>{
         event.preventDefault();
-        apiTest();
         $("#formResponse").removeClass("hidden").text("Form Submitted")
 
     })
     
 
-}
-
-function apiTest(){
-    $.post( "https://forms.hubspot.com/collected-forms/submit/form",function(){
-        alert("success")
-    })
 }
 
 function createApp(){
