83490f0e1853c31b2780ed855108b3a3a23691e5
Tess Gronski <tessgronski@gmail.com>
2020-10-10 20:57:28 -0700

    Adds server

--- data.js
+++ data.js
@@ -3,16 +3,28 @@
 function hubspotForm(){
     $("#contactForm").submit(event=>{
         event.preventDefault();
-        document.getElementById("contactForm").onclick = function() {
-            _hsq.push(["trackEvent", {
-                id: "HSFormSubmit"
-            }]);
-        };
+        addContact(event.target.firstname.value, event.target.email.value, event.target.phone.value)
         $("#formResponse").removeClass("hidden").text("Form Submitted")
 
     })
     
 
+}
+
+function addContact(firstname, email, phone){
+        return fetch("http://localhost:8000", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            firstname,
+            email,
+            phone,
+          }),
+        }).then((response)=>{
+            console.log(response.json())
+        })
 }
 
 function createApp(){
